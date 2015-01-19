c5e11daea459dcd95bb67316ef6f96dbb069c0b7
Ray Muktader <rayhan.muktader@nrtnortheast.com>
2015-01-18 19:02:17 -0500

    Removed Facebook comment section

--- sites/all/themes/humblebeggining/templates/node.tpl.php
+++ sites/all/themes/humblebeggining/templates/node.tpl.php
@@ -42,20 +42,3 @@
 
   </div>
 </article>
-
-<?php if ($page) : ?>
-
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<div class="fb-comments" 
-  style="margin-left:auto;margin-right:auto;display:block;width:470px;" 
-  data-href="<?php echo $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];?>" data-num-posts="2" data-width="470"></div>
-
-<?php endif; ?>
